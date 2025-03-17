17e534a5738fdbf4a813e225ce0c69695aeddff5
BB6689 <98437283+BB6689@users.noreply.github.com>
2025-03-17 10:09:15 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Define the path for the JSON file and the output text file
 JSON_FILE="/home/balaji/Downloads/posts-0-50-2025-03-17.json"  # Update this path to your JSON file
 OUTPUT_FILE="/home/balaji/Downloads/extrCT.txt"  # Update this path for the output text file
 
-# Check if the JSON file exists
 if [ ! -f "$JSON_FILE" ]; then
   echo "Error: JSON file '$JSON_FILE' not found!"
   exit 1
